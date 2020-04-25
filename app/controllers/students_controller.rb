c68d3ec8a88919337b444ddf404b96558b04c3d8
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-25 19:28:45 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class StudentsController < ApplicationController
   
   def index
@@ -7,5 +8,15 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+   def create
+    # binding.pry
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
+  end 
+
+  def new
+  end 
+
 
 end
